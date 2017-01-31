8df424a7eb81c74e1104df41a2c994ddd55eb5dd
Cristy Carpenter <cristy.carpenter@bigcommerce.com>
2017-01-31 16:57:17 -0600

    Added studentsPerPage and pagination function
    Added a variable to store amount of students per page - this can be
    used for calculations for the pagination.
    
    Also, I started a function for creating a pagination menu. I currently
    have it calculating the amount of page links needed.

--- pagination.js
+++ pagination.js
@@ -1,4 +1,19 @@
-//Hide all but the first 10 students on page load.
+//Store students per page for use in pagination calculations.
+var studentsPerPage = 10;
 
-$(".student-list li").slice(10).hide();
+//Show the first 10 students on page load; hide the others.
+$(".student-list li").slice(studentsPerPage).hide();
 
+//Function for creating the pagination menu.
+function createPaginationMenu() {
+    
+    //Determine the amount of students.
+    var studentCount = $(".student-list li").length;
+    console.log(studentCount); //for visibility
+    
+    //Determine how many page links are needed.
+    var pageCount = studentCount / studentsPerPage;
+    console.log(pageCount);   
+}
+
+createPaginationMenu();
